60518a11a6303308ed87a4af9ee02b927031d0d1
bioshrek <shrekwang1@gmail.com>
2014-11-13 22:51:21 +0800

    Initial Commit

---
+++ FMDBDemo/SKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SKAppDelegate.h
+//  FMDBDemo
+//
+//  Created by shrek wang on 11/13/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FMDBDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FMDBDemo
+//
+//  Created by shrek wang on 11/13/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SKAppDelegate class]));
+    }
+}
